7d5524187e4e86c47485d0d52e600b90ba9091dd
Dion Almaer <dion@almaer.com>
2010-09-27 16:02:01 -0700

    first commit

---
+++ input-speech.js
@@ -0,0 +1,10 @@
+var last_target = null;
+
+document.addEventListener('mousedown', function(event) {
+    last_target = event.target;
+}, true);
+
+chrome.extension.onRequest.addListener(function(event) {
+    last_target.setAttribute("speech", "on");
+    last_target = null;
+})
